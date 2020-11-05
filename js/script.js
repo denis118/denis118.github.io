33b17f4835a544a0867723b98e64fbee60aadaad
Denis Tsap <ts.dios.v@gmail.com>
2020-11-05 21:31:28 +0300

    Удалил файлы завершённого проекта

--- js/script.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var hotelSearchButton = document.querySelector( '.button.hotel-search' );
-hotelSearchButton.addEventListener( 'click', openHide );
-
-function openHide () {
-  var form = document.querySelector( '.form-search' );
-  form.classList.toggle( 'hide-mode' );
-}
