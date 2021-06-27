418b2bba0907bb8e5d9f6debd9fe13e8302a2701
Denis Tsap <ts.dios.v@gmail.com>
2021-06-27 14:27:04 +0300

    Plus non-infinite slider with arrows

--- js/script.js
+++ js/script.js
@@ -40,10 +40,17 @@
           if (entry.target.closest('.slider') && entry.target.closest('li')) {
             entry.target.closest('li').classList.add('visible');
           }
+
+          var currentSlider = entry.target.closest('.slider');
+          var slides = Array.from(currentSlider.querySelectorAll('.slider__item'));
+          var index = slides.indexOf(entry.target.closest('.slider__item'));
+
+          window.slideSwitching.setSlideIndex(index);
+          window.slideSwitching.switchArrowVisibility(currentSlider);
         });
       }
 
-      var mediaObserver = new IntersectionObserver(callback, mediaObserver);
+      var mediaObserver = new IntersectionObserver(callback);
 
       mediaContents.forEach(function (it) {
         mediaObserver.observe(it);
@@ -59,10 +66,18 @@
 // Slide switching by arrows
 (function () {
   var sliders = Array.from(document.querySelectorAll('.slider'));
-  var slideIndex = 0;
+  var slideIndex;
+
+  setSlideIndex();
+
+  function setSlideIndex(value) {
+    slideIndex = value || 0;
+  }
 
   sliders.forEach(function (it) {
     var arrows = Array.from(it.querySelectorAll('.slider__arrow-btn'));
+    var arrowNext = it.querySelector('.slider__arrow-btn[data-id="next"]');
+    arrowNext.classList.remove('hidden-entity');
     insertEventListeners(it, arrows);
   });
 
@@ -93,10 +108,9 @@
   function plusSlide(slider, delta) {
     var sliderList = slider.querySelector('.slider__list');
     var slides = Array.from(sliderList.querySelectorAll('.slider__item'));
-    var visibleSlides = null;
 
     if ('IntersectionObserver' in window) {
-      visibleSlides = Array.from(slider.querySelectorAll('.visible'));
+      var visibleSlides = Array.from(slider.querySelectorAll('.visible'));
 
       if (visibleSlides.length > 1) {
         return;
@@ -106,19 +120,36 @@
     }
 
     slideIndex += delta;
-    scrollSlide(sliderList, slides, slideIndex);
+    scrollSlide(slider, sliderList, slides);
   }
 
-  function scrollSlide(sliderList, slides, index) {
-    if (index > slides.length - 1) {
-      slideIndex = 0;
-    }
-
-    if (index < 0) {
-      slideIndex = slides.length - 1;
-    }
+  function scrollSlide(slider, sliderList, slides) {
+    switchArrowVisibility(slider);
 
     var scrollPos = slideIndex * (sliderList.scrollWidth / slides.length);
     sliderList.scrollLeft = scrollPos;
   }
+
+  function switchArrowVisibility(slider) {
+    var slides = Array.from(slider.querySelectorAll('.slider__item'));
+    var arrowPrevious = slider.querySelector('.slider__arrow-btn[data-id="previous"]');
+    var arrowNext = slider.querySelector('.slider__arrow-btn[data-id="next"]');
+
+    if (slideIndex === slides.length - 1 || slideIndex > slides.length - 1) {
+      arrowNext.classList.add('hidden-entity');
+    } else {
+      arrowNext.classList.remove('hidden-entity');
+    }
+
+    if (slideIndex === 0 || slideIndex < 0) {
+      arrowPrevious.classList.add('hidden-entity');
+    } else {
+      arrowPrevious.classList.remove('hidden-entity');
+    }
+  }
+
+  window.slideSwitching = {
+    setSlideIndex: setSlideIndex,
+    switchArrowVisibility: switchArrowVisibility
+  };
 })();
