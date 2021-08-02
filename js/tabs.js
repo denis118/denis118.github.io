f9f97454dbdd2cfe31e367fb5da19015d4f719eb
Denis Tsap <ts.dios.v@gmail.com>
2021-08-02 12:27:24 +0300

    Добавляет табы

---
+++ js/tabs.js
@@ -0,0 +1,103 @@
+'use strict';
+
+//
+// utility
+//
+
+(function () {
+  // monade
+  var Maybe = function (operand) {
+    this.operand = operand;
+  };
+
+  Maybe.prototype.map = function (operator) {
+    if (this.operand && operator) {
+      return new Maybe(operator(this.operand));
+    } else {
+      return new Maybe(null);
+    }
+  };
+
+  // export
+  window.utility = {
+    Maybe: Maybe
+  };
+})();
+
+//
+// tabs
+//
+
+(function () {
+  var tabs = null;
+
+  var findTabs = function () {
+    var Maybe = window.utility.Maybe;
+    tabs = new Maybe(document.querySelectorAll('.tabs'));
+    tabs = tabs.operand.length
+      ? Array.from(tabs.operand)
+      : null;
+  };
+
+  findTabs();
+
+  if (tabs.length) {
+    var onControlsClick = function (evt) {
+      if (!(evt.target.matches('.tabs__anchor-link')
+          || evt.target.parentElement.matches('.tabs__anchor-link'))) {
+        return;
+      }
+
+      evt.preventDefault();
+
+      var anchor = null;
+
+      if (evt.target.matches('.tabs__anchor-link')) {
+        anchor = evt.target;
+      }
+
+      if (evt.target.parentElement.matches('.tabs__anchor-link')) {
+        anchor = evt.target.parentElement;
+      }
+
+      var visibleContent = anchor
+          .closest('.tabs')
+          .querySelector('.tabs__content.active');
+
+      var targetContent = anchor
+          .closest('.tabs')
+          .querySelector(anchor.getAttribute('href'));
+
+      if (Object.is(visibleContent, targetContent)) {
+        return;
+      }
+
+      visibleContent.classList.remove('active');
+      targetContent.classList.add('active');
+    };
+
+    var setEventListeners = function (element) {
+      element
+          .querySelector('.tabs__controls')
+          .addEventListener('click', onControlsClick);
+    };
+
+    var eraseEventListeners = function (element) {
+      element
+          .querySelector('.tabs__controls')
+          .removeEventListener('click', onControlsClick);
+    };
+
+    tabs.forEach(function (it) {
+      setEventListeners(it);
+    });
+
+    var onWindowBeforeunload = function () {
+      tabs.forEach(function (it) {
+        eraseEventListeners(it);
+      });
+    };
+
+    window.addEventListener('beforeunload', onWindowBeforeunload);
+  }
+})();
